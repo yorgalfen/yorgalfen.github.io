b6149ba6f63068174b98e8a977ae46f32e52147d
Michael Constantine <116296561+yorgalfen@users.noreply.github.com>
2022-10-24 21:20:50 -0500

    Update program.js

--- program.js
+++ program.js
@@ -1,3 +1,15 @@
+ var txtFile = new XMLHttpRequest();
+    txtFile.open("GET", "height25.csv", true);
+    txtFile.onreadystatechange = function() {
+      if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
+        if (txtFile.status === 200) {  // Makes sure it's found the file.
+          allText = txtFile.responseText;
+          lines = txtFile.responseText.split("\n"); // Will separate each line into an array
+          alert(allText);
+        }
+      }
+    }
+
 AFRAME.registerComponent("build", {
     init: function () {
         s = document.getElementById("scene");
