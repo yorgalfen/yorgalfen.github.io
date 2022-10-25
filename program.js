57600e670030981c2653a006a4cf08fe11a82829
Michael Constantine <116296561+yorgalfen@users.noreply.github.com>
2022-10-24 22:55:33 -0500

    Update program.js

--- program.js
+++ program.js
@@ -1,21 +1,16 @@
- var txtFile = new XMLHttpRequest();
-    txtFile.open("GET", "height25.csv", true);
-    txtFile.onreadystatechange = function() {
-      if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
-        if (txtFile.status === 200) {  // Makes sure it's found the file.
-          allText = txtFile.responseText;
-          lines = txtFile.responseText.split("\n"); // Will separate each line into an array
-          alert(allText);
-        }
-      }
-    }
+$.get('height25.csv',{},function(content){
+    let lines=content.split(',');
 
+    console.log(`File contains ${lines.length} lines`);
+    console.log(`First line : ${lines[0]}`);
+
+});
 AFRAME.registerComponent("build", {
     init: function () {
         s = document.getElementById("scene");
         var testBox = document.createElement("a-box");
         testBox.setAttribute("position", "-1 0.5 -3");
-        testBox.setAttribute("rotation", "0 45 0");
+        testBox.setAttribute("rotation", "0 45 0");     
         testBox.setAttribute("material", "src: #lava");
         s.appendChild(testBox);
     }
