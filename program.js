e7ec6d31df9fbda26711d0a5bc084d0810d7c608
Michael Constantine <116296561+yorgalfen@users.noreply.github.com>
2022-10-26 19:28:43 -0500

    Update program.js

--- program.js
+++ program.js
@@ -12,31 +12,36 @@ $(document).keydown(function (){
     }
 });
 $.get('height25.csv',{},function(content){
-    height=content.split('/\r?\n/');
-    for (var i = 0; i < height.length; i++){    
+    height=content.split('\r\n');
+    for (var i = 0; i < height.length; i++){
         height[i] = height[i].split(",");
     }
+    height.pop();
     console.log(height);
 });
 $.get('slope25.csv',{},function(content){
-    slope=content.split('/\r?\n/');
+    slope=content.split('\r\n');
     for (var i = 0; i < slope.length; i++){    
         slope[i] = slope[i].split(",");
+        
     }
+    slope.pop();
     console.log(slope);
 });
 $.get('lat25.csv',{},function(content){
-    lat=content.split('/\r?\n/');
+    lat=content.split('\r\n');
     for (var i = 0; i < lat.length; i++){    
         lat[i] = lat[i].split(",");
     }
+    lat.pop();
     console.log(lat);
 });
 $.get('long25.csv',{},function(content){
-    long=content.split('/\r?\n/');
+    long=content.split('\r\n');
     for (var i = 0; i < long.length; i++){    
         long[i] = long[i].split(",");
     }
+    long.pop();
     console.log(long);
 });
 AFRAME.registerComponent("build", {
