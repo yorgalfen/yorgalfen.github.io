bc96260d7a85ebae5aaac3e063c972496e8aea55
Michael Constantine <116296561+yorgalfen@users.noreply.github.com>
2022-11-12 21:02:37 -0600

    Update program.js

--- program.js
+++ program.js
@@ -51,7 +51,7 @@ $(document).keydown(function (){
     }
     if (event.which == 67){ // C
         let c = indexes($("#camera").attr("position").x, $("#camera").attr("position").z);
-        alert(`Your position is approximately ${lat[c[0]][c[1]]}, ${long[c[0]][c[1]]}.\nYour elevation is approximately ${height[c[0]][c[1]]}.\nYour data position is row ${c[0]}, column ${c[1]}.`);
+        alert(`Your position is approximately ${lat(c[0],c[1])}, ${long(c[0],c[1])}.\nYour elevation is approximately ${height[c[0]][c[1]]}.\nYour data position is row ${c[0]}, column ${c[1]}.`);
     }
     if (event.which == 88){ // X
         let ne = prompt("Input a new rendering size. Must be a whole number, divisible by 2.");
