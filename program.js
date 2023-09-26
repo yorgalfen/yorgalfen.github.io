cd7225858c974c49215de6f9572b863ca04de279
Michael Constantine <116296561+yorgalfen@users.noreply.github.com>
2023-09-26 08:31:39 -0500

    Fix various lints

--- program.js
+++ program.js
@@ -1,9 +1,16 @@
-var height, latl, latr, longl, longr, slope, route, comms;
-var ah = false;
-var n = 0;
-var los = 5;
-var hes = 15;
-var siz = 64;
+let height;
+let latl;
+let latr;
+let longl;
+let longr;
+let slope;
+let route;
+let comms;
+let ah = false;
+let n = 0;
+let los = 5;
+let hes = 15;
+let siz = 64;
 const centerLat = -85.3974303;
 const centerLong = 30.5974913;
 const earthLat = -6.6518153;
@@ -17,33 +24,42 @@ const earthCart = {
 const r = 1737400;
 // perf: 50-70ms idles, up to 300ms
 // Ideal: <16ms
-function update_data(){
-    let c = indexes(
+function update_data() {
+    const c = indexes(
         document.querySelector("#camera").object3D.position.x,
-        document.querySelector("#camera").object3D.position.z
+        document.querySelector("#camera").object3D.position.z,
     );
-    let la = toRad(lat(c[0], c[1]));
-    let lo = toRad(long(c[0], c[1]));
-    let ra = height[c[0]][c[1]] + r;
-    let az = (bearing(lat(c[0], c[1]), long(c[0], c[1]), earthLat, 0) * 180) / Math.PI;
-    let pc = spheToCart(la, lo, ra);
-    let dpos = { x: earthCart.x - pc.x, y: earthCart.y - pc.y, z: earthCart.z - pc.z };
-    let rn = Math.hypot(dpos.x, dpos.y, dpos.z);
-    let rz =
+    const la = toRad(lat(c[0], c[1]));
+    const lo = toRad(long(c[0], c[1]));
+    const ra = height[c[0]][c[1]] + r;
+    const az = (bearing(lat(c[0], c[1]), long(c[0], c[1]), earthLat, 0) * 180) / Math.PI;
+    const pc = spheToCart(la, lo, ra);
+    const dpos = { x: earthCart.x - pc.x, y: earthCart.y - pc.y, z: earthCart.z - pc.z };
+    const rn = Math.hypot(dpos.x, dpos.y, dpos.z);
+    const rz =
         dpos.x * Math.cos(la) * Math.cos(lo) +
         dpos.y * Math.cos(la) * Math.sin(lo) +
         dpos.z * Math.sin(la);
-    let ele = (Math.asin(rz / rn) * 180) / Math.PI;
-    $("#data").html(`Press H for help.<br>Position: ${-1*lat(c[0], c[1])}&deg; S, ${long(c[0], c[1])}&deg; E<br>Height: ${height[c[0]][c[1]]} meters<br>Azimuth to Earth: ${az.toFixed(2)}&deg;<br>Elevation to Earth: ${ele.toFixed(2)}&deg;<br>Data indices: row ${c[0]}, column ${c[1]}.`);
+    const ele = (Math.asin(rz / rn) * 180) / Math.PI;
+    $("#data").html(
+        `Press H for help.<br>Position: ${-1 * lat(c[0], c[1])}&deg; S, ${long(
+            c[0],
+            c[1],
+        )}&deg; E<br>Height: ${height[c[0]][c[1]]} meters<br>Azimuth to Earth: ${az.toFixed(
+            2,
+        )}&deg;<br>Elevation to Earth: ${ele.toFixed(2)}&deg;<br>Data indices: row ${
+            c[0]
+        }, column ${c[1]}.`,
+    );
 }
 function update_scene() {
-    let start = new Date();
-    let ex = document.querySelector("#camera").object3D.position.x;
-    let ez = document.querySelector("#camera").object3D.position.z;
-    let f = indexes(ex, ez);
+    const start = new Date();
+    const ex = document.querySelector("#camera").object3D.position.x;
+    const ez = document.querySelector("#camera").object3D.position.z;
+    const f = indexes(ex, ez);
     n++;
-    for (var z = f[0] - siz / 2; z < f[0] + siz / 2; z++) {
-        for (var x = f[1] - siz / 2; x <= f[1] + siz / 2; x++) {
+    for (let z = f[0] - siz / 2; z < f[0] + siz / 2; z++) {
+        for (let x = f[1] - siz / 2; x <= f[1] + siz / 2; x++) {
             if ($(`#${z}-${x}-top`).length) {
                 update(z, x);
             } else {
@@ -52,7 +68,7 @@ function update_scene() {
         }
     }
     $(`.${n - 1}`).remove();
-    let time = new Date() - start;
+    const time = new Date() - start;
     console.log(`Spent ${time}ms in update_scene`);
 }
 $(document).keydown(function () {
@@ -67,18 +83,19 @@ $(document).keydown(function () {
                 document.querySelector("#camera").object3D.position.y -= 0.5;
             }
             break;
-        case 80: // P
-            let npo = prompt("Go to position? View help page for info.");
+        case 80: {
+            // P
+            const npo = prompt("Go to position? View help page for info.");
             if (npo) {
                 ah = false;
                 clearInterval(interv);
                 clearInterval(interv1);
                 if (npo.charAt(0) === "-") {
-                    let f = npo.split(" ");
-                    let dela = parseFloat(f[0]);
-                    let delo = parseFloat(f[1]);
+                    const f = npo.split(" ");
+                    const dela = parseFloat(f[0]);
+                    const delo = parseFloat(f[1]);
                     let q;
-                    let c = gcdisu(centerLat, centerLong, dela, delo);
+                    const c = gcdisu(centerLat, centerLong, dela, delo);
                     let ab = bearing(centerLat, centerLong, dela, delo);
                     if (ab >= 0 && ab < Math.PI / 2) {
                         q = 1;
@@ -112,12 +129,12 @@ $(document).keydown(function () {
                             b = Math.abs(b);
                             break;
                     }
-                    let nsu = 1160 + Math.round(a / vdis);
-                    let nind = 1215 + Math.round(b / hdis);
-                    let g = [nsu, nind];
+                    const nsu = 1160 + Math.round(a / vdis);
+                    const nind = 1215 + Math.round(b / hdis);
+                    const g = [nsu, nind];
                     n++;
-                    for (var z = g[0] - siz / 2; z < g[0] + siz / 2; z++) {
-                        for (var x = g[1] - siz / 2; x <= g[1] + siz / 2; x++) {
+                    for (let z = g[0] - siz / 2; z < g[0] + siz / 2; z++) {
+                        for (let x = g[1] - siz / 2; x <= g[1] + siz / 2; x++) {
                             if ($(`#${z}-${x}-top`).length) {
                                 $(`#${z}-${x}-top`).attr("class", n);
                                 $(`#${z}-${x}-bot`).attr("class", n);
@@ -132,14 +149,14 @@ $(document).keydown(function () {
                         coord(
                             parseFloat(lat(parseInt(g[0]), parseInt(g[1]))),
                             parseFloat(long(parseInt(g[0]), parseInt(g[1]))),
-                            parseFloat(height[parseInt(g[0])][parseInt(g[1])]) + 1.6
-                        )
+                            parseFloat(height[parseInt(g[0])][parseInt(g[1])]) + 1.6,
+                        ),
                     );
                 } else {
-                    let f = npo.split(" ");
+                    const f = npo.split(" ");
                     n++;
-                    for (var z = parseInt(f[0]) - siz / 2; z < parseInt(f[0]) + siz / 2; z++) {
-                        for (var x = parseInt(f[1]) - siz / 2; x <= parseInt(f[1]) + siz / 2; x++) {
+                    for (let z = parseInt(f[0]) - siz / 2; z < parseInt(f[0]) + siz / 2; z++) {
+                        for (let x = parseInt(f[1]) - siz / 2; x <= parseInt(f[1]) + siz / 2; x++) {
                             if ($(`#${z}-${x}-top`).length) {
                                 $(`#${z}-${x}-top`).attr("class", n);
                                 $(`#${z}-${x}-bot`).attr("class", n);
@@ -154,23 +171,28 @@ $(document).keydown(function () {
                         coord(
                             parseFloat(lat(parseInt(f[0]), parseInt(f[1]))),
                             parseFloat(long(parseInt(f[0]), parseInt(f[1]))),
-                            parseFloat(height[parseInt(f[0])][parseInt(f[1])]) + 1.6
-                        )
+                            parseFloat(height[parseInt(f[0])][parseInt(f[1])]) + 1.6,
+                        ),
                     );
                 }
                 interv = setInterval(update_scene, 10000);
                 interv1 = setInterval(update_data, 2000);
             }
             break;
+        }
         case 67: // C
             // nothing yet
             break;
-        case 88: // X
-            let ne = prompt("Input a new rendering size. Must be a whole number, divisible by 2.");
+        case 88: {
+            // X
+            const ne = prompt(
+                "Input a new rendering size. Must be a whole number, divisible by 2.",
+            );
             if (ne) {
                 siz = parseInt(ne);
             }
             break;
+        }
         case 76: // L
             ah = !ah;
             if (ah) {
@@ -182,24 +204,32 @@ $(document).keydown(function () {
         case 72: // H
             window.open("help.html", "_blank");
             break;
-        case 77: // M
-            let me = prompt("Input a new slope above which squares will be marked in red.");
+        case 77: {
+            // M
+            const me = prompt("Input a new slope above which squares will be marked in red.");
             if (me) {
                 hes = parseFloat(me);
             }
             break;
-        case 78: // N
-            let mv = prompt("Input a new slope below which squares will be marked in green.");
+        }
+        case 78: {
+            // N
+            const mv = prompt("Input a new slope below which squares will be marked in green.");
             if (mv) {
                 los = parseFloat(mv);
             }
             break;
-        case 86: // V
-            let fov = prompt("Input a new field of view, in degrees, for the camera. Default is 80.");
-            if (fov){
+        }
+        case 86: {
+            // V
+            const fov = prompt(
+                "Input a new field of view, in degrees, for the camera. Default is 80.",
+            );
+            if (fov) {
                 $("#camera").attr("camera", `far: 1000000000; fov: ${fov}`);
             }
             break;
+        }
         default: // W, A, S, D
             if (
                 (event.which === 65 ||
@@ -208,9 +238,9 @@ $(document).keydown(function () {
                     event.which === 68) &&
                 ah
             ) {
-                let d = indexes(
+                const d = indexes(
                     document.querySelector("#camera").object3D.position.x,
-                    document.querySelector("#camera").object3D.position.z
+                    document.querySelector("#camera").object3D.position.z,
                 );
                 document.querySelector("#camera").object3D.position.y =
                     parseFloat($(`#${d[0]}-${d[1]}-bot`).attr("vertex-c").split(" ")[1]) + 1.6;
@@ -241,35 +271,35 @@ function long(su, ind) {
     }
 }
 function gcdis(la1, lo1, la2, lo2) {
-    let dlat = toRad(la2 - la1);
-    let dlon = toRad(lo2 - lo1);
-    let lr1 = toRad(la1);
-    let lr2 = toRad(la2);
-    let a =
+    const dlat = toRad(la2 - la1);
+    const dlon = toRad(lo2 - lo1);
+    const lr1 = toRad(la1);
+    const lr2 = toRad(la2);
+    const a =
         Math.sin(dlat / 2) * Math.sin(dlat / 2) +
         Math.sin(dlon / 2) * Math.sin(dlon / 2) * Math.cos(lr1) * Math.cos(lr2);
     return r * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 }
 function gcdisu(la1, lo1, la2, lo2) {
-    let dlat = toRad(la2 - la1);
-    let dlon = toRad(lo2 - lo1);
-    let lr1 = toRad(la1);
-    let lr2 = toRad(la2);
-    let a =
+    const dlat = toRad(la2 - la1);
+    const dlon = toRad(lo2 - lo1);
+    const lr1 = toRad(la1);
+    const lr2 = toRad(la2);
+    const a =
         Math.sin(dlat / 2) * Math.sin(dlat / 2) +
         Math.sin(dlon / 2) * Math.sin(dlon / 2) * Math.cos(lr1) * Math.cos(lr2);
     return 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 }
 function bearing(startLat, startLng, destLat, destLng) {
-    startLat = toRad(startLat);
-    startLng = toRad(startLng);
-    destLat = toRad(destLat);
-    destLng = toRad(destLng);
+    const startLat = toRad(startLat);
+    const startLng = toRad(startLng);
+    const destLat = toRad(destLat);
+    const destLng = toRad(destLng);
     y = Math.sin(destLng - startLng) * Math.cos(destLat);
     x =
         Math.cos(startLat) * Math.sin(destLat) -
         Math.sin(startLat) * Math.cos(destLat) * Math.cos(destLng - startLng);
-    let brng = Math.atan2(y, x);
+    const brng = Math.atan2(y, x);
     return brng;
 }
 function spheToCart(la, lo, ra) {
@@ -280,24 +310,24 @@ function spheToCart(la, lo, ra) {
     };
 }
 function coord(la, lo, he) {
-    let b = bearing(centerLat, centerLong, la, lo);
-    let g = gcdis(centerLat, centerLong, la, lo);
-    let sgr = Math.sin(g / r);
-    let x = (r + he) * sgr * Math.sin(b);
-    let y = he * Math.sqrt(1 - sgr * sgr) - 2 * r * Math.pow(Math.sin(g / (2 * r)), 2);
-    let z = (r + he) * sgr * Math.cos(b);
+    const b = bearing(centerLat, centerLong, la, lo);
+    const g = gcdis(centerLat, centerLong, la, lo);
+    const sgr = Math.sin(g / r);
+    const x = (r + he) * sgr * Math.sin(b);
+    const y = he * Math.sqrt(1 - sgr * sgr) - 2 * r * Math.pow(Math.sin(g / (2 * r)), 2);
+    const z = (r + he) * sgr * Math.cos(b);
     return `${x.toFixed(3)} ${y.toFixed(3)} ${z.toFixed(3)}`;
 }
 function tri(subList, index) {
     let col;
-    let a = coord(lat(subList, index), long(subList, index), height[subList][index]);
-    let c = coord(lat(subList - 1, index), long(subList - 1, index), height[subList - 1][index]);
-    let b = coord(
+    const a = coord(lat(subList, index), long(subList, index), height[subList][index]);
+    const c = coord(lat(subList - 1, index), long(subList - 1, index), height[subList - 1][index]);
+    const b = coord(
         lat(subList - 1, index + 1),
         long(subList - 1, index + 1),
-        height[subList - 1][index + 1]
+        height[subList - 1][index + 1],
     );
-    let d = coord(lat(subList, index + 1), long(subList, index + 1), height[subList][index + 1]);
+    const d = coord(lat(subList, index + 1), long(subList, index + 1), height[subList][index + 1]);
     if (slope[subList][index] >= hes) {
         col = "red";
     } else {
@@ -318,8 +348,8 @@ function tri(subList, index) {
     </a-triangle><a-triangle id="${subList}-${index}-bot" class="${n}" vertex-a="${a}" vertex-b="${b}" vertex-c="${d}" material="side: double; roughness: 1" src="#home-${col}"></a-triangle>`);
 }
 function update(z, x) {
-    let fid = `${z}-${x}`;
-    let slo = slope[z][x];
+    const fid = `${z}-${x}`;
+    const slo = slope[z][x];
     $(`#${fid}-top`).attr("class", n);
     $(`#${fid}-bot`).attr("class", n);
     if (!route.includes(fid)) {
@@ -337,19 +367,20 @@ function update(z, x) {
 }
 function indexes(camx, camz) {
     if ($("a-triangle[id*=top]").length) {
-        let sub, ind;
+        let sub;
+        let ind;
         $("a-triangle[id*=top]").each(function () {
             id = $(this).attr("id");
             sub = parseInt(id.slice(0, id.indexOf("-")));
             ind = parseInt(id.slice(id.indexOf("-") + 1, id.lastIndexOf("-")));
-            let a = $(this).attr("vertex-a").split(" ").map(parseFloat);
-            let b = $(this).attr("vertex-b").split(" ").map(parseFloat);
-            let c = $(this).attr("vertex-c").split(" ").map(parseFloat);
-            let d = $(`#${sub}-${ind}-bot`).attr("vertex-c").split(" ").map(parseFloat);
-            let minz = Math.min(a[2], b[2], c[2], d[2]);
-            let minx = Math.min(a[0], b[0], c[0], d[0]);
-            let maxz = Math.max(a[2], b[2], c[2], d[2]);
-            let maxx = Math.max(a[0], b[0], c[0], d[0]);
+            const a = $(this).attr("vertex-a").split(" ").map(parseFloat);
+            const b = $(this).attr("vertex-b").split(" ").map(parseFloat);
+            const c = $(this).attr("vertex-c").split(" ").map(parseFloat);
+            const d = $(`#${sub}-${ind}-bot`).attr("vertex-c").split(" ").map(parseFloat);
+            const minz = Math.min(a[2], b[2], c[2], d[2]);
+            const minx = Math.min(a[0], b[0], c[0], d[0]);
+            const maxz = Math.max(a[2], b[2], c[2], d[2]);
+            const maxx = Math.max(a[0], b[0], c[0], d[0]);
             if (camx >= minx && camx <= maxx && camz >= minz && camz <= maxz) {
                 return false;
             }
@@ -361,7 +392,7 @@ function indexes(camx, camz) {
 }
 
 async function start() {
-    let start = new Date();
+    const start = new Date();
 
     const assets = [
         ["height.json", "height"],
@@ -373,19 +404,19 @@ async function start() {
         ["comms.json", "comms"],
         ["slope.json", "slope"],
     ];
-    let promises = assets.map((item) =>
+    const promises = assets.map((item) =>
         fetch(item[0])
             .then((response) => response.json())
             .then((content) => {
                 window[item[1]] = content;
-            })
+            }),
     );
 
     await Promise.all(promises).catch((error) => {
         console.error(error.message);
     });
 
-    let time = new Date() - start;
+    const time = new Date() - start;
     console.log(`Spent ${time}ms in init()`);
     interv = setInterval(update_scene, 10000);
     interv1 = setInterval(update_data, 2000);
